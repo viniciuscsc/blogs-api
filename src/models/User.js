5e56cf63dfcc8c15931d885f13f18756c9d56cf1
cunhavcs <cunha.vcs@gmail.com>
2023-06-27 15:43:27 -0300

    Req. 02: cria o modelo User em src/models/User.js

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+  });
+
+  return userTable;
+};
